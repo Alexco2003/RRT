4ec135f4a13276ce22698824b251e164a49fef56
Codarcea Alexandru-Christian <alexandru.codarcea@gmail.com>
2025-04-08 21:55:56 +0300

    added configuration

--- formatImage.py
+++ formatImage.py
@@ -2,22 +2,22 @@ from PIL import Image, ImageOps
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 img = Image.open('test_images/test.png')
+
+fixed_size = (1800, 900)
+img = img.resize(fixed_size)
+
 img = ImageOps.grayscale(img)
 
 np_img = np.array(img)
+
+
 np_img = ~np_img
 np_img[np_img > 0] = 1
+
 plt.set_cmap('binary')
 plt.imshow(np_img)
-
-# Save Image
-np.save('test_images/test.npy', np_img)
-
-#Read Image
-grid = np.load('test_images/test.npy')
-print(grid)
-print(grid.shape)
-plt.imshow(grid, cmap = "binary")
-plt.tight_layout()
 plt.show()
+
+np.save('test_images/test.npy', np_img)
--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from matplotlib.pyplot import rcParams
 np.set_printoptions(precision=3, suppress=True)
 rcParams['font.family'] = 'sans-serif'
 rcParams['font.sans-serif'] = ['Tahoma']
-plt.rcParams['font.size'] = 22
+plt.rcParams['font.size'] = 12
 
 class Node:
     def __init__(self, x, y):
@@ -34,7 +34,63 @@ class RRT:
     def __repr__(self):
         return f"RRT({self.randomTree}, {self.goal}, {self.grid}, {self.iterations}, {self.distance})"
 
+    # add the point to the nearest node and add goal when reached
+    def addChild(self, x, y):
+        pass
+
+    # get a random point in the grid
+    def randomPoint(self):
+        pass
+
+    # steer a distance from the start point to the end point
+    def steerToPoint(self, start, end):
+        pass
+
+    # check if the obstacle is in the path
+    def isInObstacle(self, start, end):
+        pass
+
+    # find unit vector between two points
+    def unitVector(self, start, end):
+        pass
+
+    # find the nearest node from a given unconnected point (Euclidian distance)
+    def findNearestNode(self, root, point):
+        pass
+
+    # find euclidian distance between a node and a point
+    def distance(self, node1, point):
+        pass
+
+    # check if the goal has been reached
+    def goalFound(self, point):
+        pass
+
+    # reset the nearest node and distance
+    def resetNearestValues(self):
+        pass
+
+    # trace the path from goal to start
+    def retraceRRTPath(self, goal):
+        pass
 
 
+# Load the grid, set start and goal <x, y> positions, number of iterations, step size
+grid = np.load('test_images/test.npy')
+start = np.array([100.0, 100.0])
+goal = np.array([1700.0,750.0])
+numIterations = 200
+stepSize = 50
+goalRegion = plt.Circle((goal[0], goal[1]), stepSize, color='b', fill=False)
+
+fig = plt.figure("RRT Algorithm")
+plt.imshow(grid, cmap='binary')
+plt.plot(start[0], start[1], 'ro')
+plt.plot(goal[0], goal[1], 'bo')
+ax = fig.gca()
+ax.add_patch(goalRegion)
+plt.xlabel('X-axis $(m)$')
+plt.ylabel('Y-axis $(m)$')
+plt.show()
 
 
